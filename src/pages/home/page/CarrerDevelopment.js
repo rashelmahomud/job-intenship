8b21611d8a47caa1f87e947602e9bcf6994a5783
MCTC <rashelmahmudraj1998@gmail.com>
2023-01-27 06:15:54 -0800

    adde design

--- src/pages/home/page/CarrerDevelopment.js
+++ src/pages/home/page/CarrerDevelopment.js
@@ -19,16 +19,17 @@ const CarrerDevelopment = () => {
             </div>
             <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 lg:gap-10 gap-5'>
                 {
-                    carers.map(carer => <div className='relative hover:border-4' key={carer._id}>
+                    carers.map(carer => <div className='hover:relative delay-300 duration-100 hover:border-4 border-teal-200' key={carer._id}>
                         <img  src={carer.img} alt='' />
-                        <div className='text-center bg-gray-100 opacity-75 hover:opacity-100 lg:px-10 p-5 absolute bottom-0 left-0'>
+                        <div className='text-center bg-gray-100 opacity-75 hover:opacity-100 lg:px-10 p-5 absolute bottom-0 '>
                             <h1 className='font-semibold text-gray-500'>{carer.name}</h1>
                             <h3 className='text-xl font-semibold my-3'>{carer.title}</h3>
-                            <Link to='/'>Reacd More</Link>
+                            <Link to='/' className='text-blue-600'>Reacd More</Link>
                         </div>
                     </div>)
                 }
             </div>
+            <p className='text-center mt-5'><Link to='/' className='text-blue-600 hover:border-b text-center'>View More Articals</Link></p>
         </div>
     );
 };

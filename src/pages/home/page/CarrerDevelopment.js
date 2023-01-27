29805355883f984670df3d2bbcf968c54f5e07e6
MCTC <rashelmahmudraj1998@gmail.com>
2023-01-27 05:08:36 -0800

    added some work

--- src/pages/home/page/CarrerDevelopment.js
+++ src/pages/home/page/CarrerDevelopment.js
@@ -17,11 +17,11 @@ const CarrerDevelopment = () => {
                 <h1 className='font-semibold'>SEE HOW YOU CAN UP YOUR CARRER START</h1>
                 <h3 className='text-4xl font-semibold'>CAREER DEVELOPMENT & TRAINING</h3>
             </div>
-            <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 lg:gap-10 gap-5  '>
+            <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 lg:gap-10 gap-5'>
                 {
-                    carers.map(carer => <div className='relative' key={carer._id}>
+                    carers.map(carer => <div className='relative hover:border-4' key={carer._id}>
                         <img  src={carer.img} alt='' />
-                        <div className='text-center bg-gray-100 lg:p-10 p-5 absolute '>
+                        <div className='text-center bg-gray-100 opacity-75 hover:opacity-100 lg:px-10 p-5 absolute bottom-0 left-0'>
                             <h1 className='font-semibold text-gray-500'>{carer.name}</h1>
                             <h3 className='text-xl font-semibold my-3'>{carer.title}</h3>
                             <Link to='/'>Reacd More</Link>
